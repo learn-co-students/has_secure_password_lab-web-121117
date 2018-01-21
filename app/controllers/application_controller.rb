496809c0ebcfcc4277c6823df8ad4dc6bbdd8436
github username <github email address>
2018-01-21 13:06:28 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
    def current_user
-     @user = (User.find_by(id: session[:user_id]) || User.new)
+     @user = User.find_by(id: session[:user_id])
    end
 
    def logged_in?
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
    end
 
    def require_logged_in
-     return redirect_to '/login' unless logged_in?
+     redirect_to '/' unless logged_in?
    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:user][:name])
-    user = user.try(:authenticate, params[:user][:password])
-    if user
-      session[:user_id] = user.id
-      @user = user
+    @user = User.find_by(name: params[:user][:name])
+    check_user = @user.try(:authenticate, params[:user][:password])
+    if check_user
+      session[:user_id] = check_user.id
+      @user = check_user
       redirect_to '/'
     else
       redirect_to '/login'
